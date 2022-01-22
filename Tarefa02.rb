db45d33e4c5e68ad1b3dff5dc3442a2275db4dd8
Felipevidal21 <89077573+Felipevidal21@users.noreply.github.com>
2022-01-22 17:45:32 -0300

    Delete Tarefa02.rb

--- Tarefa02.rb
+++
@@ -1,3 +0,0 @@
-print "digite um nome : " # Saida de dados que pede para digitar o nome, sem quebrar a linha
-nome = gets.chomp         #recebe os dados digitados pelo teclado e armazena na variavel "nome"
-puts "Seu Nome na ordem inversa é : #{nome.reverse}" # função "reverse" vai inverter a o "nome" 
