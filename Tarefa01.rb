a9597513f9ad2729d194f2ec2ea4a1c62fc2e0f4
Felipevidal21 <89077573+Felipevidal21@users.noreply.github.com>
2022-01-22 17:44:45 -0300

    Delete Tarefa01.rb

--- Tarefa01.rb
+++
@@ -1,5 +0,0 @@
-print "digite um nome" # Saida de dados que pede para digitar o nome,sem quebrar a linha
-nome = gets.chomp     #recebe os dados digitados pelo teclado e armazena na variavel "nome"
-puts "Seu Nome é : #{nome}" #mostra a saida dados ,com o nome armazenado
-puts "comprimento do Nome é : #{nome.size}" #mostra a saida dados ,com o tamanho variavel
-
